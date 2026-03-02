348b290a53751fda58eec9103c69df1084e24077
Bening-DA <amerueee@gmail.com>
2026-03-02 08:54:09 +0700

    Add files via upload

---
+++ pyhton/tugas/in.py
@@ -0,0 +1,12 @@
+# Parent Class
+class Hewan:
+ def makan(self):
+   print("Hewan ini sedang makan.")
+# Child Class (Mewarisi Hewan)
+class Burung(Hewan):
+ def terbang(self):
+   print("Burung sedang terbang.")
+# Object
+beo = Burung()
+beo.makan() # Mewarisi dari Hewan (Output: Hewan ini sedang makan.)
+beo.terbang() # Method milik sendiri
